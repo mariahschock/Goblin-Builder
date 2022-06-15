8ceebf403611769b6657ddc6db3ab60162b04b7c
Mariah <mariahschock@gmail.com>
2022-06-15 16:21:41 -0700

    for testing

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
         }}/>
       </div>
       <div className='goblin-filter quarter'>
-        Filter Goblins
+        Filter Your Goblins
         {/* note that handleFilterGoblins is defined upstairs. This is where the allGoblins array gets filtered */}
         <input onChange={(e) => handleFilterGoblins(e.target.value)} />
       </div>
